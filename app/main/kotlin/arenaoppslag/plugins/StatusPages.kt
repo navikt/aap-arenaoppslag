2d44a8d56d2837ecb46dadda7362eec88067d5dc
Fredrik Meyer <fredrik.meyer@nav.no>
2024-12-04 16:07:55 +0100

    Returner JSON ved feil

--- app/main/kotlin/arenaoppslag/plugins/StatusPages.kt
+++ app/main/kotlin/arenaoppslag/plugins/StatusPages.kt
@@ -10,14 +10,18 @@ import org.slf4j.LoggerFactory
 private val logger = LoggerFactory.getLogger("App")
 private val secureLog: Logger = LoggerFactory.getLogger("secureLog")
 
+data class FeilRespons(
+    val melding: String
+)
+
 fun Application.statusPages() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             logger.error("Uhåndtert feil. Se sikker logg for stack trace.")
             secureLog.error("Uhåndtert feil", cause)
-            call.respondText(
-                text = "Feil i tjeneste: ${cause.message}",
-                status = HttpStatusCode.InternalServerError
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                FeilRespons("Feil i tjeneste: ${cause.message}"),
             )
         }
     }
