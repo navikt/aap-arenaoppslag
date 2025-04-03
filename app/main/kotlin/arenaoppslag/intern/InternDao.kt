a77c71c5e2034394d03bc8b2b5f2bb59b7aabab4
VHollund <vetle.hollund@nav.no>
2025-04-03 08:51:21 +0200

    fjerner forrige linje for å gjennskape feilen

--- app/main/kotlin/arenaoppslag/intern/InternDao.kt
+++ app/main/kotlin/arenaoppslag/intern/InternDao.kt
@@ -36,9 +36,10 @@ object InternDao {
                   FROM person 
                  WHERE fodselsnr = ?) 
            AND rettighetkode = 'AAP'
-           AND (fra_dato <= til_dato OR til_dato IS NULL)
            AND vedtaktypekode IN ('O', 'E', 'G')
     """
+
+    // AND (fra_dato <= til_dato OR til_dato IS NULL)
 
     private const val hentBeregningsgrunnlag = """
         SELECT vedtakfaktakode, vedtakverdi
