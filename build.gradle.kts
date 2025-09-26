b5e136d117db912b7bb1d18e52e87817eced1d27
Per Henden <per.christian.henden@nav.no>
2025-09-26 11:00:30 +0200

    Korreksjoner for gradle. Pga feil som trigges av strengere parsing i Gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 
 plugins {
     kotlin("jvm") version "2.2.20"
@@ -19,20 +19,39 @@ subprojects {
         withType<Test> {
             reports.html.required.set(false)
             useJUnitPlatform()
-            maxParallelForks = Runtime.getRuntime().availableProcessors()
+            maxParallelForks = Runtime.getRuntime().availableProcessors() / 2
         }
+
+        (findByName("distTar") as? Tar)?.apply {
+            // Bruk et unikt navn for jar-filen til distTar, for å unngå navnekollisjoner i multi-modul prosjekt,
+            // slik at vi ikke bruker samme navn, feks. "kontrakt.jar" "api.jar" i flere moduler.
+            // Dette unngår feil av typen "Entry <name>.jar is a duplicate but no duplicate handling strategy has been set"
+            archiveBaseName.set("${rootProject.name}-${project.name}")
+        }
+
+        kotlin.sourceSets["main"].kotlin.srcDirs("main/kotlin")
+        kotlin.sourceSets["test"].kotlin.srcDirs("test/kotlin")
+        sourceSets["main"].resources.srcDirs("main/resources")
+        sourceSets["test"].resources.srcDirs("test/resources")
+
+        kotlin {
+            jvmToolchain(21)
+            explicitApi = ExplicitApiMode.Warning
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+                apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
+                languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
+            }
+
+        }
+
     }
 
-    kotlin.sourceSets["main"].kotlin.srcDirs("main/kotlin")
-    kotlin.sourceSets["test"].kotlin.srcDirs("test/kotlin")
-    sourceSets["main"].resources.srcDirs("main/resources")
-    sourceSets["test"].resources.srcDirs("test/resources")
-
-    kotlin {
-        jvmToolchain(21)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_21)
-            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
-        }
+    // Pass på at når vi kaller JavaExec eller Test tasks så bruker vi samme JVM som vi kompilerer med
+    val toolchainLauncher = javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
+    tasks.withType<Test>().configureEach { javaLauncher.set(toolchainLauncher) }
+    tasks.withType<JavaExec>().configureEach { javaLauncher.set(toolchainLauncher) }
+
 }
