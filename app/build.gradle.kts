2d592a66badf42fbcd21176c8d9780699ccfee26
Per Henden <per.christian.henden@nav.no>
2025-11-03 13:58:50 +0100

    fix: gradle-oppsett som i behandlingsflyt
    Følg samme mønster som behandlingsflyt i de andre appene som publiserer jar-filer
    
    AAP-1627

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.kotlin.dsl.withType
+
 plugins {
+    id("aap.conventions")
+    id("com.gradleup.shadow") version "9.2.2"
     id("io.ktor.plugin") version "3.3.1"
     application
 }
@@ -43,4 +48,33 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.27.6")
     testImplementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     testImplementation("com.h2database:h2:2.4.240")
+}
+
+tasks {
+
+    withType<ShadowJar> {
+        // Duplikate class og ressurs-filer kan skape runtime-feil, fordi JVM-en velger den første på classpath
+        // ved duplikater, og det kan være noe annet enn vår kode (og libs vi bruker) forventer.
+        // Derfor logger vi en advarsel hvis vi oppdager duplikater.
+        duplicatesStrategy = DuplicatesStrategy.WARN
+
+        mergeServiceFiles()
+
+        filesMatching(listOf("META-INF/io.netty.*", "META-INF/services/**", "META-INF/maven/**")) {
+            // For disse filene fra upstream, antar vi at de er identiske hvis de har samme navn.
+            // Merk at META-INF/maven/org.webjars/swagger-ui/pom.properties
+            // brukes av com.papsign.ktor.openapigen.SwaggerUIVersion
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+            // Vi beholder alle pom.properties fra Maven for å støtte generering av SBOM i Nais
+        }
+
+        // Helt unødvendige filer som ofte skaper duplikater
+        val fjernDisseDuplikatene = listOf(
+            "*.SF", "*.DSA", "*.RSA", // Signatur-filer som ikke trengs på runtime
+            "*NOTICE*", "*LICENSE*", "*DEPENDENCIES*", "*README*", "*COPYRIGHT*", // til mennesker bare
+            "proguard/**", // Proguard-konfigurasjoner som ikke trengs på runtime
+            "com.android.tools/**" // Android build-filer som ikke trengs på runtime
+        )
+        fjernDisseDuplikatene.forEach { pattern -> exclude("META-INF/$pattern") }
+    }
 }
