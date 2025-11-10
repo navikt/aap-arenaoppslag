76d097b8c4f7059edd159bc3e825036cd638541a
Per Henden <per.christian.henden@nav.no>
2025-11-10 10:20:42 +0100

    Fix: copy-pasta av postgres config

--- app/main/kotlin/arenaoppslag/datasource/ArenaDatasource.kt
+++ app/main/kotlin/arenaoppslag/datasource/ArenaDatasource.kt
@@ -5,22 +5,10 @@ import arenaoppslag.prometheus
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import java.sql.ResultSet
-import java.util.Properties
 import javax.sql.DataSource
 
 
 internal object Hikari {
-    private val postgresConfig = Properties().apply {
-        put("tcpKeepAlive", true) // kreves av Hikari
-
-        put("socketTimeout", 300) // sekunder, makstid for overføring av svaret fra db
-        put("statement_timeout", 300_000) // millisekunder, makstid for db til å utføre spørring
-
-        put("logUnclosedConnections", true) // vår kode skal lukke alle connections
-        put("logServerErrorDetail", false) // ikke lekk person-data fra queries etc til logger ved feil
-
-        put("assumeMinServerVersion", "16.0") // raskere oppstart av driver
-    }
 
     fun create(dbConfig: DbConfig): DataSource =
         HikariDataSource(HikariConfig().apply {
@@ -35,7 +23,6 @@ internal object Hikari {
             driverClassName = dbConfig.driver
             connectionTestQuery = "SELECT 1 FROM DUAL"
             metricRegistry = prometheus
-            dataSourceProperties = postgresConfig
             maximumPoolSize = 10
         })
 }
