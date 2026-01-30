2ffb3f72c14659cf5326c0a9dbbe829b3e80957e
Per Henden <per.christian.henden@nav.no>
2026-01-30 10:25:11 +0100

    AAP-1823

--- app/src/test/kotlin/no/nav/aap/arenaoppslag/database/HistorikkRepositoryTest.kt
+++ app/src/test/kotlin/no/nav/aap/arenaoppslag/database/HistorikkRepositoryTest.kt
@@ -1,6 +1,5 @@
 package no.nav.aap.arenaoppslag.database
 
-import no.nav.aap.arenaoppslag.database.HistorikkRepository.Companion.historiskeRettighetkoderIArena
 import no.nav.aap.arenaoppslag.kontrakt.intern.ArenaSak
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -29,49 +28,43 @@ class HistorikkRepositoryTest : H2TestBase("flyway/minimumtest", "flyway/eksiste
     }
 
     @Test
-    fun `ingen saker for person med kun vedtak på historiske rettighetkoder`() {
+    fun `ingen saker for person med kun veldig gamle vedtak`() {
         val testPerson = "kun_gamle"
         val testPersonId = 992
         val alleSaker:List<ArenaSak> = sakRepository.hentSaker(testPerson)
-        val kunHistoriske =
-            alleSaker.map { it.rettighetkode }.filter { it in historiskeRettighetkoderIArena }
-        assertThat(kunHistoriske).hasSize(2)
+        assertThat(alleSaker).hasSize(2)
 
         val relevanteSaker = historikkRepository.hentAlleSignifikanteSakerForPerson(testPersonId, testDato)
         assertThat(relevanteSaker).isEmpty()
     }
 
     @Test
-    fun `finner saker for person med vedtak på kun nye rettighetkoder`() {
+    fun `finner saker for person med kun nye vedtak`() {
         val testPersonFnr = "kun_nye"
         val testPersonId = 996
         val alleSaker = sakRepository.hentSaker(testPersonFnr)
-        val kunHistoriske =
-            alleSaker.map { it.rettighetkode }.filter { it in historiskeRettighetkoderIArena }
-        assertThat(kunHistoriske).isEmpty() // ingen historiske koder
+        assertThat(alleSaker).hasSize(3)
+
+        val relevanteSaker = historikkRepository.hentAlleSignifikanteSakerForPerson(testPersonId, testDato)
+        assertThat(relevanteSaker).hasSize(4)
+    }
+
+    @Test
+    fun `finner saker for person med både gamle og nye vedtak`() {
+        val testPersonFnr = "blanding"
+        val testPersonId = 997
+        val alleSaker = sakRepository.hentSaker(testPersonFnr)
+        assertThat(alleSaker).hasSize(6)
 
         val relevanteSaker = historikkRepository.hentAlleSignifikanteSakerForPerson(testPersonId, testDato)
         assertThat(relevanteSaker).hasSize(3)
     }
 
     @Test
-    fun `finner saker for person med vedtak på både nye og historiske rettighetkoder`() {
-        val testPersonFnr = "blanding"
-        val testPersonId = 997
-        val alleSaker = sakRepository.hentSaker(testPersonFnr)
-        val kunHistoriske =
-            alleSaker.map { it.rettighetkode }.filter { it in historiskeRettighetkoderIArena }
-        assertThat(kunHistoriske).hasSize(2) // noen historiske koder
-
-        val relevanteSaker = historikkRepository.hentAlleSignifikanteSakerForPerson(testPersonId, testDato)
-        assertThat(relevanteSaker).hasSize(2)
-    }
-
-    @Test
     fun `kombinert spørring for relevant historikk kjører uten feil`(){
         val testPersonId = 997
         val relevanteSaker = historikkRepository.hentAlleSignifikanteSakerForPerson(testPersonId, testDato)
-        assertThat(relevanteSaker).hasSize(2)
+        assertThat(relevanteSaker).hasSize(3)
     }
 
 }
