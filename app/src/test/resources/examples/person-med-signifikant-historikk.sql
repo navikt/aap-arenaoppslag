4b699909553e93a5b7a48a047e073fdd45477e27
Per Henden <per.christian.henden@nav.no>
2026-02-20 20:40:21 +0100

    Oppdater eksempel

--- app/src/test/resources/examples/person-med-signifikant-historikk.sql
+++ app/src/test/resources/examples/person-med-signifikant-historikk.sql
@@ -1,18 +1,23 @@
 -- Eksempelspørring for å finne ut om en person har nyere historikk i Arena
 -- som kan være signifikant for om personen kan tas inn i Kelvin eller ikke.
 
--- TODO: search-replace '?' med en person_id som finnes i Arena
-
-SELECT sak_id, vedtakstatuskode, vedtaktypekode, fra_dato, til_dato, rettighetkode
+SELECT
+    sak_id,
+    vedtakstatuskode,
+    vedtaktypekode,
+    fra_dato,
+    til_dato,
+    rettighetkode,
+    utfallkode
 FROM
     vedtak v
 
-WHERE v.person_id = ?
-  AND v.utfallkode != 'AVBRUTT'
+WHERE v.person_id = :person_id
+  AND (v.utfallkode IS NULL OR v.utfallkode != 'AVBRUTT')
   AND v.rettighetkode IN ('AA115', 'AAP')
-  AND v.MOD_DATO >= DATE '2020-01-01' -- ytelse: unngå å løpe gjennom veldig gamle vedtak
+  AND v.MOD_DATO >= ADD_MONTHS(TRUNC(SYSDATE), -72) -- ytelse: unngå å løpe gjennom veldig gamle vedtak
   AND NOT (fra_dato > til_dato AND (til_dato IS NOT NULL AND fra_dato IS NOT NULL)) -- filtrer ut ugyldiggjorte vedtak
-  AND NOT ((fra_dato IS NULL AND til_dato IS NULL) AND vedtakstatuskode NOT IN ('OPPRE', 'MOTAT', 'REGIS', 'INNST')) -- filtrer ut etterregistrerte vedtak, men behold vedtak som er under behandling
+  AND ((fra_dato IS NOT NULL OR til_dato IS NOT NULL) OR vedtakstatuskode IN ('OPPRE', 'MOTAT', 'REGIS', 'INNST')) -- filtrer ut etterregistrerte vedtak, men behold vedtak som er under behandling
   AND (
     (vedtaktypekode IN ('O','E','G') AND (til_dato >= DATE '2024-06-15' OR til_dato IS NULL)) -- vanlig tidsbuffer på 18 måneder
         OR
@@ -30,15 +35,16 @@ SELECT
     vedtaktypekode,
     CAST(NULL AS DATE)                    AS fra_dato,
     TO_DATE(vf.vedtakverdi, 'DD-MM-YYYY') AS til_dato,
-    v.rettighetkode
+    v.rettighetkode,
+    v.utfallkode
 FROM
     vedtak v
         JOIN vedtakfakta vf ON vf.vedtak_id = v.vedtak_id
 WHERE
-    v.person_id = ?
-  AND v.utfallkode != 'AVBRUTT'
+    v.person_id = :person_id
+  AND (v.utfallkode IS NULL OR v.utfallkode != 'AVBRUTT')
   AND v.rettighetkode IN ( 'KLAG1', 'KLAG2' )
-  AND v.MOD_DATO >= DATE '2020-01-01' -- ytelse: unngå å løpe gjennom veldig gamle vedtak, begrens string-til-dato konvertering
+  AND v.MOD_DATO >= ADD_MONTHS(TRUNC(SYSDATE), -72) -- ytelse: unngå å løpe gjennom veldig gamle vedtak, begrens string-til-dato konvertering
   AND vf.vedtakfaktakode = 'INNVF'
   -- Vi regner klager med null INNVF som åpne. Klager med fersk INNVF-dato regnes også som åpne, pga. det tar tid før AAP-vedtakene registreres.
   -- Og at det kan komme en ny klage eller anke etter at klagen er behandlet og avslått. Anker sjekkes for seg selv.
@@ -53,26 +59,17 @@ SELECT
     vedtakstatuskode,
     vedtaktypekode,
     CAST(NULL AS DATE)                    AS fra_dato,
-    TO_DATE(vf.vedtakverdi, 'DD-MM-YYYY') AS til_dato,
-    v.rettighetkode
+    CAST(NULL AS DATE)                    AS til_dato,
+    v.rettighetkode,
+    v.utfallkode
 FROM
     vedtak v
         JOIN vedtakfakta vf ON vf.vedtak_id = v.vedtak_id
 WHERE
-    v.person_id = ?
+    v.person_id = :person_id
+  AND (v.utfallkode IS NULL OR v.utfallkode != 'AVBRUTT')
   AND rettighetkode = 'ANKE'
-  AND utfallkode != 'AVBRUTT'
-  AND v.MOD_DATO >= DATE '2020-01-01' -- ytelse: unngå å løpe gjennom veldig gamle vedtak
-  AND vf.vedtakfaktakode = 'KJREGDATO'
-  AND ( vf.vedtakverdi IS NULL OR TO_DATE(vf.vedtakverdi, 'DD-MM-YYYY') >= ADD_MONTHS(TRUNC(SYSDATE), -36) ) -- stor tidsbuffer her, da det kan ankes oppover i rettsvesenet.
-  -- Dersom anken ble innvilget for mer enn 6 mnd siden, regnes den som ikke relevant lenger.
-  -- Vi bruker vedtakfaktakode=KJENNELSE fremfor vedtak.utfallkode=JA her, fordi vi ser uventet utfallkode for noen innvilgede anker i produksjon.
-  AND NOT EXISTS(SELECT 1 from vedtakfakta vf_innvilget
-                 WHERE vf_innvilget.vedtak_id = v.vedtak_id -- samme vedtaket
-                   AND vf_innvilget.vedtakfaktakode = 'KJENNELSE'
-                   AND vf_innvilget.vedtakverdi = 'JA' -- er innvilget (kan evt utvides med flere verdier)
-                   AND TO_DATE(vf.vedtakverdi, 'DD-MM-YYYY') <= ADD_MONTHS(TRUNC(SYSDATE), -6) -- er minst 6 mnd siden
-)
+  AND v.MOD_DATO >= ADD_MONTHS(TRUNC(SYSDATE), -72) -- ytelse: unngå å løpe gjennom veldig gamle vedtak
 
 UNION ALL
 
@@ -82,34 +79,35 @@ SELECT
     vedtaktypekode,
     CAST(NULL AS DATE)                    AS fra_dato,
     TO_DATE(vf.vedtakverdi, 'DD-MM-YYYY') AS til_dato,
-    v.rettighetkode
+    v.rettighetkode,
+    v.utfallkode
 FROM
     vedtak v
         JOIN vedtakfakta vf ON vf.vedtak_id = v.vedtak_id
 WHERE
-    v.person_id = ?
+    v.person_id = :person_id
   AND rettighetkode = 'TILBBET'
-  AND utfallkode != 'AVBRUTT'
-  AND v.MOD_DATO >= DATE '2021-01-01' -- ytelse: unngå å løpe gjennom veldig gamle vedtak
+  AND (v.utfallkode IS NOT NULL AND v.utfallkode != 'AVBRUTT')
+  AND v.MOD_DATO >= ADD_MONTHS(TRUNC(SYSDATE), -60) -- ytelse: unngå å løpe gjennom veldig gamle vedtak
   AND vf.vedtakfaktakode = 'INNVF'
-  -- Vi regner tilbakebetalinger med null INNVF som åpne, ellers ikke.
+  -- Vi regner tilbakebetalinger med null INNVF som åpne, ellers ikke
   AND vf.vedtakverdi IS NULL -- det er ikke satt endelig dato for beslutning på vedtaket
--- SPM: finnes det en dato eller annet vi kan lese for å vite når tilbakebetalingen er fullført av personen?
 
 UNION ALL
 
 SELECT
     v.sak_id,
     su.vedtakstatuskode,
-    CAST(NULL AS VARCHAR2(10))  AS vedtaktypekode,
+    'O'  AS vedtaktypekode,
     su.dato_fra AS fra_dato,
     su.dato_til AS til_dato,
-    'SPESIAL' AS rettighetkode
+    'SPESIAL' AS rettighetkode,
+    v.utfallkode
 FROM
     spesialutbetaling su
         JOIN vedtak v ON v.vedtak_id = su.vedtak_id -- for å få sak_id
 WHERE
-    su.person_id = ?
+    su.person_id = :person_id
   -- Dersom utbetalingen ikke er datofestet, eller den har skjedd nylig, regner vi saken som åpen, ellers ikke.
   -- Vi bruker en tidsbuffer her i tilfelle det klages på spesialutbetalingen etter at den er utbetalt.
   AND (su.dato_utbetaling IS NULL OR su.dato_utbetaling >= ADD_MONTHS(TRUNC(SYSDATE), -3) )
@@ -120,17 +118,16 @@ UNION ALL
 SELECT
     v.sak_id,
     v.vedtakstatuskode,
-    v.vedtakstatuskode,
+    'O'  AS vedtaktypekode,
     ssu.dato_periode_fra AS fra_dato,
     ssu.dato_periode_til AS til_dato,
-    'SIM_SPESIAL' AS rettighetkode
+    'SIM_SPESIAL' AS rettighetkode,
+    v.utfallkode
 FROM
     sim_utbetalingsgrunnlag ssu
         JOIN vedtak v ON v.vedtak_id = ssu.vedtak_id
 WHERE
-    ssu.person_id = ?
+    ssu.person_id = :person_id
   -- MERK: ingen index i sim_utbetalingsgrunnlag på mod_dato eller andre datofelt, så blir tregt
   AND ssu.mod_dato >= ADD_MONTHS(TRUNC(SYSDATE), -3) -- ignorer gamle simuleringer som ikke ble noe av
--- SPM: er denne spørringen unødvendig, vil feks. et tilhørende vedtak for personen uansett
--- finnes i Vedtak (TILBBET) eller i Spesialutbetaling?
 ;
