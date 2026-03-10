a0a249afec94020b779d61b3e0022aa4ee1967a6
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-03-10 11:10:30 +0100

    Legg til TelleverkRepository med hentTelleverkPåPerson-funksjon og tilhørende tester

---
+++ app/src/main/kotlin/no/nav/aap/arenaoppslag/database/TelleverkRepository.kt
@@ -0,0 +1,40 @@
+package no.nav.aap.arenaoppslag.database;
+
+import org.intellij.lang.annotations.Language
+import javax.sql.DataSource;
+
+
+data class KvoteVerdi(val kode: String, val verdi: Int)
+
+
+class TelleverkRepository(private val datasource: DataSource) {
+
+    companion object {
+        @Language("OracleSql")
+        internal val selectTelleverkPåPerson = """
+        SELECT verdi, beregningsleddkode
+FROM beregningsledd
+WHERE tabellnavnalias_kilde = 'KVOTBR'
+  AND person_id = ?
+  AND beregningsleddkode IN ('AAP', 'MAAPU');
+    """.trimIndent()
+
+    }
+
+
+    fun hentTelleverkPåPerson(arenaPersonId: Int): Set<KvoteVerdi> {
+        return datasource.connection.use { con ->
+            con.prepareStatement(selectTelleverkPåPerson).use { preparedStatement ->
+                preparedStatement.setInt(1, arenaPersonId)
+                val resultSet = preparedStatement.executeQuery()
+                resultSet.map { row ->
+                    KvoteVerdi(
+                        kode = row.getString("beregningsleddkode"),
+                        verdi = row.getInt("verdi")
+                    )
+                }.toSet()
+            }
+        }
+    }
+}
+
