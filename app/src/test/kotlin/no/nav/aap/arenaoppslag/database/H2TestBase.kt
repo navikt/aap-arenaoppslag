c9264c63e872c90459fa857e413a424db38ca271
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-03-11 12:33:14 +0100

    Oppdater H2TestBase for å bruke TestConfig og legg til ny migrasjonsbane i TelleverkRepositoryTest

--- app/src/test/kotlin/no/nav/aap/arenaoppslag/database/H2TestBase.kt
+++ app/src/test/kotlin/no/nav/aap/arenaoppslag/database/H2TestBase.kt
@@ -1,21 +1,13 @@
 package no.nav.aap.arenaoppslag.database
 
 import no.nav.aap.arenaoppslag.DbConfig
+import no.nav.aap.arenaoppslag.TestConfig
 import org.flywaydb.core.Flyway
 import javax.sql.DataSource
 
 abstract class H2TestBase(vararg migrationLocations: String) {
 
-    private val dbName = this::class.simpleName ?: "testdb_${System.nanoTime()}"
-
-    private val dbConfig = DbConfig(
-        username = "SA",
-        password = "",
-        url = "jdbc:h2:mem:$dbName;MODE=Oracle;DB_CLOSE_DELAY=-1",
-        driver = "org.h2.Driver"
-    )
-
-    protected val h2: DataSource = ArenaDatasource.create(dbConfig)
+    protected val h2: DataSource = ArenaDatasource.create(TestConfig.oracleH2InMem)
 
     init {
         Flyway.configure().dataSource(h2).locations("flyway/common", *migrationLocations).load().apply { migrate() }
