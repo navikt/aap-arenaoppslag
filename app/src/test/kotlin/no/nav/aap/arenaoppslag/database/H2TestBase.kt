062e02016bc44643689d1fb71ead32ae9de082c5
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-03-11 10:15:13 +0100

    Oppdater H2TestBase for å bruke DbConfig og dynamisk databasename

--- app/src/test/kotlin/no/nav/aap/arenaoppslag/database/H2TestBase.kt
+++ app/src/test/kotlin/no/nav/aap/arenaoppslag/database/H2TestBase.kt
@@ -1,15 +1,26 @@
 package no.nav.aap.arenaoppslag.database
 
-import no.nav.aap.arenaoppslag.TestConfig
+import no.nav.aap.arenaoppslag.DbConfig
 import org.flywaydb.core.Flyway
 import javax.sql.DataSource
 
 abstract class H2TestBase(vararg migrationLocations: String) {
 
-    protected val h2: DataSource = ArenaDatasource.create(TestConfig.oracleH2InMem)
+    private val dbName = this::class.simpleName ?: "testdb_${System.nanoTime()}"
+
+    private val dbConfig = DbConfig(
+        username = "SA",
+        password = "",
+        url = "jdbc:h2:mem:$dbName;MODE=Oracle;DB_CLOSE_DELAY=-1",
+        driver = "org.h2.Driver"
+    )
+
+    protected val h2: DataSource = ArenaDatasource.create(dbConfig)
 
     init {
         Flyway.configure().dataSource(h2).locations("flyway/common", *migrationLocations).load().apply { migrate() }
     }
 
 }
+
+
