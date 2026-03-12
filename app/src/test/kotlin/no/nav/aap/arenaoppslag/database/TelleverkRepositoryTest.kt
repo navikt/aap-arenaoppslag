6e3173630e0f7b6ad1f5c9cea64f9fb262183a43
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-03-12 09:29:03 +0100

    Oppdater TelleverkRepositoryTest for å bruke AssertJ i stedet for JUnit assertions

--- app/src/test/kotlin/no/nav/aap/arenaoppslag/database/TelleverkRepositoryTest.kt
+++ app/src/test/kotlin/no/nav/aap/arenaoppslag/database/TelleverkRepositoryTest.kt
@@ -1,7 +1,8 @@
 package no.nav.aap.arenaoppslag.database
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+
 
 class TelleverkRepositoryTest : H2TestBase("flyway/telleverk", "flyway/minimumtest", "flyway/eksisterer") {
 
@@ -13,8 +14,8 @@ class TelleverkRepositoryTest : H2TestBase("flyway/telleverk", "flyway/minimumte
 
         val kvote = telleverkRepository.hentTelleverkPåPerson("123")
 
-        assertEquals(2, kvote.size)
-        assertEquals(setOf(KvoteVerdi("AAP", 5280), KvoteVerdi("MAAPU", 460)), kvote)
+        assertThat(kvote).hasSize(2)
+        assertThat(kvote).isEqualTo(setOf(KvoteVerdi("AAP", 5280), KvoteVerdi("MAAPU", 460)))
     }
 
     @Test
@@ -22,7 +23,7 @@ class TelleverkRepositoryTest : H2TestBase("flyway/telleverk", "flyway/minimumte
         val telleverkRepository = TelleverkRepository(h2)
 
         val kvote = telleverkRepository.hentTelleverkPåPerson("000")
-        assertTrue(kvote.isEmpty())
+        assertThat(kvote).isEmpty()
     }
 
     @Test
@@ -35,9 +36,9 @@ class TelleverkRepositoryTest : H2TestBase("flyway/telleverk", "flyway/minimumte
         val aapPerson1 = kvotePerson1.find { it.kode == "AAP" }!!.verdi
         val aapPerson2 = kvotePerson2.find { it.kode == "AAP" }!!.verdi
 
-        assertNotEquals(aapPerson1, aapPerson2)
-        assertEquals(5280, aapPerson1)
-        assertEquals(15060, aapPerson2)
+        assertThat(aapPerson1).isNotEqualTo(aapPerson2)
+        assertThat(aapPerson1).isEqualTo(5280)
+        assertThat(aapPerson2).isEqualTo(15060)
     }
 
 }
