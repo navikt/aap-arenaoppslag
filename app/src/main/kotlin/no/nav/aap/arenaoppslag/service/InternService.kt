9e0900f1dbeca974ed79e142e3fc2bfc75de266f
Nicolas N. <nicolas.nordhagen@nav.no>
2026-03-19 09:18:53 +0100

    Cache på henting av maksimum

--- app/src/main/kotlin/no/nav/aap/arenaoppslag/service/InternService.kt
+++ app/src/main/kotlin/no/nav/aap/arenaoppslag/service/InternService.kt
@@ -1,22 +1,31 @@
 package no.nav.aap.arenaoppslag.service
 
+import com.github.benmanes.caffeine.cache.Caffeine
+import io.micrometer.core.instrument.binder.cache.CaffeineCacheMetrics
+import java.time.Duration
+import java.time.LocalDate
+import no.nav.aap.arenaoppslag.Metrics.prometheus
 import no.nav.aap.arenaoppslag.database.MaksimumRepository
 import no.nav.aap.arenaoppslag.database.PeriodeRepository
-import no.nav.aap.arenaoppslag.database.PersonRepository
-import no.nav.aap.arenaoppslag.database.TelleverkRepository
 import no.nav.aap.arenaoppslag.database.VedtakRepository
 import no.nav.aap.arenaoppslag.kontrakt.intern.PerioderMed11_17Response
 import no.nav.aap.arenaoppslag.kontrakt.intern.SakStatus
 import no.nav.aap.arenaoppslag.kontrakt.intern.VedtakResponse
 import no.nav.aap.arenaoppslag.kontrakt.modeller.Maksimum
-import no.nav.aap.arenaoppslag.modeller.TellerverkPåPerson
-import java.time.LocalDate
 
 class InternService(
     private val maksimumRepository: MaksimumRepository,
     private val periodeRepository: PeriodeRepository,
-    private val vedtakRepository: VedtakRepository
+    private val vedtakRepository: VedtakRepository,
 ) {
+    private val maksimumCache = Caffeine.newBuilder()
+        .maximumSize(10_000)
+        .expireAfterWrite(Duration.ofMinutes(15))
+        .build<String, Maksimum>()
+
+    init {
+        CaffeineCacheMetrics.monitor(prometheus, maksimumCache, "arenaoppslag_maksimum")
+    }
 
     fun hentPerioder(fodselsnr: String, fraOgMedDato: LocalDate, tilOgMedDato: LocalDate): VedtakResponse {
         val hentPerioder = periodeRepository.hentPerioder(
@@ -43,10 +52,11 @@ class InternService(
         return vedtak.map { SakStatus(it.sakId, it.statusKode, it.periode, it.kilde) }
     }
 
-    fun hentMaksimum(fodselsnr: String, fraOgMedDato: LocalDate, tilOgMedDato: LocalDate): Maksimum {
-        return maksimumRepository.hentMaksimumsløsning(
-            fodselsnr, fraOgMedDato, tilOgMedDato
-        ).tilKontrakt()
-    }
+    fun hentMaksimum(fodselsnr: String, fraOgMedDato: LocalDate, tilOgMedDato: LocalDate): Maksimum =
+        maksimumCache.get("$fodselsnr-$fraOgMedDato-$tilOgMedDato") {
+            maksimumRepository.hentMaksimumsløsning(
+                fodselsnr, fraOgMedDato, tilOgMedDato
+            ).tilKontrakt()
+        }
 
 }
