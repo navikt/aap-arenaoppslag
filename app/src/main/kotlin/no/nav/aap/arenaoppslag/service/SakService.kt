6c627e73b77c6fc2323fda8d79320cba0408df15
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-06-04 13:54:51 +0200

    refactor: remove outdated comments from hentMaksdatoAapForVedtakISaker method

--- app/src/main/kotlin/no/nav/aap/arenaoppslag/service/SakService.kt
+++ app/src/main/kotlin/no/nav/aap/arenaoppslag/service/SakService.kt
@@ -49,16 +49,6 @@ class SakService(private val sakRepository: SakRepository) {
         val maksdatoene = hentMaksdatoAapForVedtakISaker(personId)
         val sisteSak = maksdatoene
             .filter { it.sisteVedtak.maxdatoAap != null }.maxByOrNull { it.sisteVedtak.maxdatoAap!! }
-        /** Maksdato er funner basert på reglen:
-         * Finner siste AAP-vedtak for denne brukeren
-         * Finner sak knytet til dette vedtaket
-         * Hvis løpende vedtak. Returner beregnet maksdato
-         * Hvis sak som har gått til maks: Returner maksdato
-         * Hvis siste vedtak er stansvedtak: Returnere null
-         * Hvis vi ikke finner noen relevante saker: Returnere null
-         */
-
-
         if (sisteSak == null || !sisteSak.lopendeVedtak) return null
 
         return sisteSak.sisteVedtak.maxdatoAap
