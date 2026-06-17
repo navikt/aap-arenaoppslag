9a24cbf25fd83f6125393b3cb22a3d46e33a45fc
Per Henden <per.christian.henden@nav.no>
2026-06-17 11:14:22 +0200

    ignorer stansede vedtak som er avsluttet
    De skal ikke regnes som signifikant historikk som hindrer en søknad å gå til Kelvin, i seg selv.

--- app/src/main/kotlin/no/nav/aap/arenaoppslag/database/HistorikkRepository.kt
+++ app/src/main/kotlin/no/nav/aap/arenaoppslag/database/HistorikkRepository.kt
@@ -56,7 +56,7 @@ class HistorikkRepository(private val dataSource: DataSource) {
           AND ( 
                 (vedtaktypekode IN ('O','E','G') AND (til_dato IS NULL OR til_dato >= ?)) -- vanlig tidsbuffer på 18 måneder
                   OR
-                (vedtaktypekode = 'S' AND (fra_dato IS NULL OR fra_dato >= ?)) -- ekstra tidsbuffer for Stans, som bare har fra_dato
+                (vedtaktypekode = 'S' AND vedtakstatuskode != 'AVSLU' AND (fra_dato IS NULL OR fra_dato >= ?)) -- ekstra tidsbuffer for Stans, som bare har fra_dato
               )
           AND NOT (utfallkode = 'NEI' AND til_dato IS NULL AND (fra_dato IS NOT NULL AND fra_dato <= ?)) -- utfallkode NEI vil ha åpen til_dato, så ekskluder disse når de er gamle
         """.trimIndent()
@@ -84,7 +84,7 @@ class HistorikkRepository(private val dataSource: DataSource) {
           AND ( 
                 (vedtaktypekode IN ('O','E','G') AND (til_dato IS NULL OR til_dato >= ?)) -- vanlig tidsbuffer på 18 måneder
                   OR
-                (vedtaktypekode = 'S' AND (fra_dato IS NULL OR fra_dato >= ?)) -- ekstra tidsbuffer for Stans, som bare har fra_dato
+                (vedtaktypekode = 'S' AND vedtakstatuskode != 'AVSLU' AND (fra_dato IS NULL OR fra_dato >= ?)) -- ekstra tidsbuffer for Stans, som bare har fra_dato
               )
           AND NOT (utfallkode = 'NEI' AND til_dato IS NULL) -- bruker fikk avslag
         """.trimIndent()
