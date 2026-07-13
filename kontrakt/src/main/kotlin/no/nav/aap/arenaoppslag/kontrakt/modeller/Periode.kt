a1450d0bf3396f387e181373e8f1c4b0d3ef31ce
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-13 10:24:43 +0200

    Fjern validering på periode slik at api-intern kan motta ugyldige vedtak

--- kontrakt/src/main/kotlin/no/nav/aap/arenaoppslag/kontrakt/modeller/Periode.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/arenaoppslag/kontrakt/modeller/Periode.kt
@@ -5,12 +5,4 @@ import java.time.LocalDate
 public data class Periode(
     val fraOgMedDato: LocalDate?,
     val tilOgMedDato: LocalDate?
-) {
-    init {
-        if (fraOgMedDato != null && tilOgMedDato != null && tilOgMedDato != fraOgMedDato) {
-            require(tilOgMedDato.isAfter(fraOgMedDato)) {
-                "Til og med dato må være etter fra og med dato { fraOgMedDato=$fraOgMedDato, tilOgMedDato=$tilOgMedDato }"
-            }
-        }
-    }
-}
+)
