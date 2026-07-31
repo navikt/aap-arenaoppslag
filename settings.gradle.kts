c8ef12b6c4b442a8e10414b5b26fc3009d296d1b
Fredrik Meyer <fredrik.meyer@nav.no>
2026-07-31 14:50:01 +0200

    Nyeste gradle-versjon + fiks deprecation warnings i Gradle-setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,6 @@ plugins {
 rootProject.name = "arenaoppslag"
 include("app", "kontrakt")
 
-
-val githubPassword: String? by settings
-
 dependencyResolutionManagement {
     // Felles for alle gradle prosjekter i repoet
     @Suppress("UnstableApiUsage")
@@ -16,23 +13,6 @@ dependencyResolutionManagement {
         maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
         mavenCentral()
         maven("https://packages.confluent.io/maven/")
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/navikt/behandlingsflyt")
-            credentials {
-                username = "x-access-token"
-                password = (githubPassword
-                    ?: System.getenv("GITHUB_PASSWORD")
-                    ?: System.getenv("GITHUB_TOKEN")
-                    ?: "").apply {
-                    if (this.isBlank()) {
-                        // Log as error instead of failing the build.
-                        // This works around the GHA Automatic Dependency Submission (Gradle) validate-project step not passing on ENV values
-                        logger.error("Either GITHUB_TOKEN or GITHUB_PASSWORD must be set in env to download NAV packages")
-                    }
-                }
-            }
-        }
         mavenLocal()
     }
 }
