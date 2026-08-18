527100d860e96a522e26cc81864a1e16b18bf72e
Per Henden <per.christian.henden@nav.no>
2026-08-18 17:19:50 +0200

    whitespace

--- app/src/main/kotlin/no/nav/aap/arenaoppslag/Metrics.kt
+++ app/src/main/kotlin/no/nav/aap/arenaoppslag/Metrics.kt
@@ -25,27 +25,21 @@ object Metrics {
         Tag.of("aktfase", ettVedtak.aktivitetsfaseKode)
     )
 
-    fun MeterRegistry.registrerEnsligSignifikantVedtak(
-        eneste: ArenaVedtak
-    ) {
+    fun MeterRegistry.registrerEnsligSignifikantVedtak(eneste: ArenaVedtak) {
         this.counter(
             "arenaoppslag_eneste_signifikante_vedtak",
             taggListeForVedtak(eneste)
         ).also { counter -> counter.increment() }
     }
 
-    fun MeterRegistry.registrerNyesteSignifikanteVedtak(
-        siste: ArenaVedtak
-    ) {
+    fun MeterRegistry.registrerNyesteSignifikanteVedtak(siste: ArenaVedtak) {
         this.counter(
             "arenaoppslag_nyeste_signifikante_vedtak",
             taggListeForVedtak(siste)
         ).also { counter -> counter.increment() }
     }
 
-    fun MeterRegistry.registrerAntallSignifikanteVedtak(
-        antall: Int
-    ) {
+    fun MeterRegistry.registrerAntallSignifikanteVedtak(antall: Int) {
         val bucket = if (antall >= 9) "9+" else antall.toString()
         this.counter(
             "arenaoppslag_signifikante_vedtak_antall",
