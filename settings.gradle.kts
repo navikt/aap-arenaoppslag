6a096dc0c0eef0e7d46ba38f3a9802943b66b5b4
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-19 13:52:09 +0200

    AAP-BAU Følg Gradle best practices
    https://docs.gradle.org/9.7.0/userguide/best_practices_index.html

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+pluginManagement {
+    includeBuild("build-logic")
+}
+
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
@@ -10,9 +14,18 @@ dependencyResolutionManagement {
     // Felles for alle gradle prosjekter i repoet
     @Suppress("UnstableApiUsage")
     repositories {
-        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release") {
+            // Speiler kun no.nav-artefakter, så vi slipper å slå opp tredjeparts-avhengigheter her
+            content { includeGroupByRegex("no\\.nav.*") }
+        }
         mavenCentral()
-        maven("https://packages.confluent.io/maven/")
+        maven("https://packages.confluent.io/maven/") {
+            // Kun Confluent/Avro-avhengigheter hentes herfra
+            content {
+                includeGroupByRegex("io\\.confluent.*")
+                includeGroup("org.apache.avro")
+            }
+        }
         mavenLocal()
     }
 }
